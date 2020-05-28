f44b3b72f1743e7a28c193e4c3b4d2d1b6f819af
Kasper Jacobsen <kjitwork@gmail.com>
2020-05-28 18:13:49 +0200

    Setting everything up

---
+++ DataLayer/Entities/Players.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Entities
+{
+    public class Players
+    {
+        public int PlayerID { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
