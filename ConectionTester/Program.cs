400db8f54fa5214c8a6b83e2bb8735c1dabf759d
Kasper Jacobsen <kjitwork@gmail.com>
2020-05-29 13:25:37 +0200

    Setting up the project enviorments

---
+++ ConectionTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConectionTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
