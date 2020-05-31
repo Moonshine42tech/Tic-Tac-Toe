3921114590829a6762ef08aafe33e54229fe4cd8
Kasper Jacobsen <kjitwork@gmail.com>
2020-05-31 13:06:18 +0200

    models on the clien side has been made. as well as references from top layer to repositorys

---
+++ TTT_Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TTT_Models
+{
+    public class Player
+    {
+        public string DisplayName { get; set; }
+        public bool SingleOrMultiPlayer { get; set; }
+        public int AmountOfGames { get; set; }
+    }
+}
---
+++ TTT_Models/ScoreBoard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TTT_Models
+{
+    public class ScoreBoard
+    {
+        public int Win { get; set; }                    // Amount of Wins
+        public int Tie { get; set; }                    // Amount of Ties
+        public int Lose { get; set; }                   // Amount of Loses
+
+        /// <summary>
+        /// Updates the game score on the scoreboard
+        /// </summary>
+        /// <param name="gameResult">The result of a single game</param>
+        public void SetScore(int gameResult)
+        {
+            try
+            {
+                switch (gameResult)
+                {
+                    case 1:
+                        Win = (Win + 1);
+                        break;
+
+                    case 2:
+                        Tie = (Tie + 1);
+                        break;
+
+                    case 3:
+                        Lose = (Lose + 1);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                // Catches a null exeption if the 'gameResult' is null
+            }
+            catch (Exception)
+            {
+                // Catches most unacountet exeptions
+            }
+
+        }
+    }
+}
