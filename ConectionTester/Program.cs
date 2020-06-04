034fa6e865676400c77791aedad83082114f7dde
Kasper Jacobsen <kasp837c@elevcampus.dk>
2020-06-04 14:03:13 +0200

    Building and testing a test server and client for websocets

--- ConectionTester/Program.cs
+++ ConectionTester/Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using TTT_Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ConectionTester
 {
-    class Program
+    static class Program
     {
-
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-
-            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
-
     }
 }
